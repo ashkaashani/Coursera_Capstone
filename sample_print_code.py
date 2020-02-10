c1bea786ffb3a95e28e311aab7a68416f3b30258
Ashka Ashani <Ashka.Ashani@tatacommunications.com>
2020-02-10 15:26:35 +0530

    Adding sample print code

---
+++ sample_print_code.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 10 15:17:24 2020
+
+@author: AAshani
+
+Sample python code for Capstone data science course on coursera for pushing to Github
+
+"""
+
+import pandas as pd
+import numpy as np
+print("Hello Capstone Project Course!")
